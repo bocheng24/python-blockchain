8cbc20ede21551086448ce7652a570023e3807bb
bocheng24 <jeffcgp@hotmail.com>
2024-12-15 14:09:22 +0800

    genesis block and fake mining function

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,7 +1,49 @@
+import time
+
+def mine_block(last_block, data):
+
+    ts = time.time_ns()
+    hash = last_block.timestamp - ts
+
+    block = Block(ts, last_block.hash, hash, data)
+    
+    return block
+
+
+def genesis():
+    ts = 1
+    last_hash = 'genesis_last_hash'
+    hash = 'genesis_hash'
+    data = []
+    
+    return Block(ts, last_hash, hash, data)
+
 class Block:
 
-    def __init__(self, data):
+    def __init__(self, timestamp, lasthash, hash, data):
+        self.timestamp = timestamp
+        self.lasthash = lasthash
+        self.hash = hash
         self.data = data
 
     def __repr__(self):
-        return f'Block | Data: {self.data}'
+        return (
+            'Block (\n'
+            f'\tTimestamp: {self.timestamp}\n'
+            f'\tlasthash: {self.lasthash}\n'
+            f'\thash: {self.hash}\n'
+            f'\tData: {self.data}\n'
+            ')'
+        )
+
+
+def main():
+    genesis_block = genesis()
+
+    next_block = mine_block(genesis_block, 'hello')
+
+    print(genesis_block)
+    print(next_block)
+
+if __name__ == '__main__':
+    main()
