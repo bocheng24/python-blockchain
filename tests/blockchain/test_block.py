e27032fdeb26682ef10f7f98d00c8286c3933481
bocheng24 <jeffcgp@hotmail.com>
2024-12-19 11:15:56 +0800

    test case for adjust_difficulty

--- tests/blockchain/test_block.py
+++ tests/blockchain/test_block.py
@@ -1,4 +1,7 @@
 from blockchain.block import Block
+from time import sleep, time_ns
+
+from configs.config import MINE_RATE, SECONDS
 
 
 def test_mine_block():
@@ -22,3 +25,38 @@ def test_genesis():
     assert genesis_block.lasthash == 'genesis_last_hash'
     assert genesis_block.hash == 'genesis_hash'
     assert genesis_block.data == []
+
+
+def test_quickly_mined_block_difficulty():
+    last_block = Block.mine_block(Block.genesis(), 'one')
+    next_block = Block.mine_block(last_block, 'two')
+
+    assert next_block.difficulty == last_block.difficulty + 1
+
+
+def test_slowly_mined_block_difficulty():
+    last_block = Block.mine_block(Block.genesis(), 'one')
+
+    sleep(MINE_RATE / SECONDS)
+
+    next_block = Block.mine_block(last_block, 'two')
+
+    assert next_block.difficulty == last_block.difficulty - 1
+
+
+def test_difficulty_at_1():
+    last_block = Block(
+        timestamp=time_ns(),
+        lasthash='last_hash',
+        hash='hash',
+        data='diffculty 1 data',
+        difficulty=1,
+        nonce=0
+    )
+
+    sleep(MINE_RATE / SECONDS)
+
+    next_block = Block.mine_block(last_block, 'two')
+
+    assert next_block.difficulty == last_block.difficulty
+    assert next_block.difficulty == 1
